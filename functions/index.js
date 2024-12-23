532ab0ccaed3a8bfe124abbf85b7d2b96773c840
kpusz <kpusz@novomatic-tech.com>
2024-12-23 23:23:22 +0000

    VV-4 - restore onUserDelete function

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,33 @@ admin.initializeApp();
 const db = admin.firestore();
 const storage = admin.storage();
 
+// Funkcja obsługująca usuwanie wszystkich plików użytkownika po jego usunięciu
+exports.onUserDelete = functions.firestore.onDocumentDeleted(
+  "users/{userId}",
+  async (event) => {
+    const userId = event.params.userId;
+    const userFolderPath = `${userId}/`;
+
+    try {
+      const [files] = await storage
+        .bucket()
+        .getFiles({ prefix: userFolderPath });
+
+      const deletePromises = files.map((file) => file.delete());
+      await Promise.all(deletePromises);
+
+      console.log(
+        `Wszystkie pliki i foldery dla użytkownika ${userId} zostały usunięte.`
+      );
+    } catch (error) {
+      console.error(
+        `Błąd podczas usuwania plików dla użytkownika ${userId}:`,
+        error
+      );
+    }
+  }
+);
+
 // Funkcja zwiększająca licznik po dodaniu pliku
 exports.onRecordingUpload = functions.storage.onObjectFinalized(
   async (event) => {
