8c07744ec301b9176ac534a88735ca7b8bee9334
kpusz <kpusz@novomatic-tech.com>
2024-12-23 18:59:06 +0000

    VV-4 - clean comment from main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'providers/connectivity_provider.dart'; // Import ConnectivityProvider
+import 'providers/connectivity_provider.dart';
 import 'screens/splash_screen.dart';
 
 void main() async {
